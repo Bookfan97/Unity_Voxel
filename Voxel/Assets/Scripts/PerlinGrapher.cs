bf0d11ae7566440c23bfd59b6b78c35053d7867f
Noah Day <sheltielover432@gmail.com>
2021-10-05 00:44:35 -0400

    Fractal Brownian Motion

--- Voxel/Assets/Scripts/PerlinGrapher.cs
+++ Voxel/Assets/Scripts/PerlinGrapher.cs
@@ -8,6 +8,9 @@ public class PerlinGrapher : MonoBehaviour
 {
     public LineRenderer lineRenderer;
     public float heightScale =2f, scale = 0.5f;
+    public int octaves = 1;
+    public float heightOffset = 1;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +19,29 @@ public class PerlinGrapher : MonoBehaviour
         Graph();
     }
 
+    float fBM(float x, float z)
+    {
+        float total = 0;
+        float frequency = 1;
+        for (int i = 0; i < octaves; i++)
+        {
+            total += Mathf.PerlinNoise(x * scale * frequency, z * scale * frequency) * heightScale;
+            frequency *= 2;
+        }
+
+        return total;
+    }
+    
     private void Graph()
     {
+        lineRenderer = this.GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 100;
         int z = 11;
         Vector3[] positions = new Vector3[lineRenderer.positionCount];
         for (int x = 0; x < lineRenderer.positionCount; x++)
         {
-            float y = Mathf.PerlinNoise(x * scale, z* scale) * heightScale;
+            float y = fBM(x, z) + heightOffset; 
+                //Mathf.PerlinNoise(x * scale, z* scale) * heightScale;
             positions[x] = new Vector3(x, y, z);
         }
         lineRenderer.SetPositions(positions);
